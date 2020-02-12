f403c50933c7a200fcf8ef4bfe2815e0b850e9a4
avidler <a.vidler@live.co.uk>
2020-02-12 21:28:24 +0000

    commit on master

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [level, setLevel] = useState(1)
+  const [numString, setNumString] = useState("")
+  const [isTimeRunning, setIsTimeRunning] = useState(false)
+
+  function generateRandomNumber(level) {
+    let tempNumString = ""
+    for (let x = 0; x < level; x++){
+      tempNumString = tempNumString + (Math.floor(Math.random() * 10))
+    }
+    setLevel(level+1)
+    setNumString(tempNumString)
+  }
+
+  function resetGame(){
+    setLevel(1)
+    setNumString("")
+    setIsTimeRunning(false)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Check your memory</h1>
+      <button onClick={num => generateRandomNumber(level)}>Click for number</button>
+      <p>{numString}</p>
+      <button onClick={resetGame}>Reset Game</button>
     </div>
   );
 }
---
+++ src/Components/game.js
@@ -0,0 +1,10 @@
+import React, {useState} from "react"
+
+function game() {
+
+
+}
+
+
+
+export default game
