4de34e5a57e692d5b22023455150b0b9b713feaa
avidler <a.vidler@live.co.uk>
2020-02-18 19:17:26 +0000

    attempt to connect to database 3

--- backend/server.js
+++ backend/server.js
@@ -13,19 +13,6 @@ app.use(express.json())
 
 
 const uri = process.env.ATLAS_URI
-
-const config = require('../../config/mongo');
-const env = process.env.NODE_ENV || 'development';
-const localUrl = 'mongodb://${config[env].host}/${config[env].dbName}';
-const prodUrl = config[env]['env-variable'];
-const connectionUrl = prodUrl ? prodUrl : localUrl;
-//const connect = mongoose.connect(connectionUrl, {
-//useNewUrlParser: true
-//});
-module.exports = connect;
-
-
-
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
 
 
