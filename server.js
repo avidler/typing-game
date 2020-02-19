0065140d2ce07cda9973be71fccd8c9df41f27ee
avidler <a.vidler@live.co.uk>
2020-02-19 17:40:19 +0000

    heroku deploy v2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-require('dotenv').config()
+require('dotenv').config({ path: '.env' })
 
 const path = require('path')
 
